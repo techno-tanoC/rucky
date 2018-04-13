f4d171aff8631d3ba89293102e1834eefbfd22b3
techno-tanoC <develop.tanoc@gmail.com>
2018-04-13 21:04:23 +0900

    use the token if user specify the token

--- src/req.rs
+++ src/req.rs
@@ -2,15 +2,29 @@ use reqwest;
 use reqwest::header::*;
 use serde::de::DeserializeOwned;
 
-use page::*;
-
 pub struct Req;
 
 impl Req {
     pub fn get<D: DeserializeOwned>(url: &str, token: &Option<String>) -> Option<D> {
-        reqwest::get(url)
-            .and_then(|mut res| res.json())
-            .ok()
+        if token.is_some() {
+            let access_token = Bearer {
+                token: token
+                    .clone()
+                    .expect("the token must not be empty")
+                    .to_string()
+            };
+
+            reqwest::Client::new()
+                .get(url)
+                .header(Authorization(access_token))
+                .send()
+                .and_then(|mut res| res.json())
+                .ok()
+        } else {
+            reqwest::get(url)
+                .and_then(|mut res| res.json())
+                .ok()
+        }
     }
 
     pub fn auth_get<D: DeserializeOwned>(url: &str, token: &Option<String>) -> Option<D> {
